2c45f305f5a87fa548ad5088192e9b0437497536
Riddhi <riddhisoni686@gmail.com>
2026-08-08 02:52:21 -0400

    streamlit setup + homescreen

---
+++ src/components/footer.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def footer_home():
+
+    logo_url = "https://i.ibb.co/YTYGn5qV/logo.png"
+
+    st.markdown(f""" 
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+            <p style="font-weight:bold; color:white"> Created with ❤️ by RIDDHI SONI </p>
+            
+        </div>
+
+    """,unsafe_allow_html=True)
