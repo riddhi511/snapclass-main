844f8eba1cb00f164358227936b332eddfe96e11
Riddhi <riddhisoni686@gmail.com>
2026-08-09 18:30:38 -0400

    coded

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -5,7 +5,7 @@ import io
 
 @st.dialog("Share Class Link")
 def share_subject_dialog(subject_name, subject_code):
-    app_domain = "http://localhost:8501"
+    app_domain = "https://snapclass-main.streamlit.app"
     join_url = f"{app_domain}/?join_code={subject_code}"
 
     st.header("Scan to Join")
