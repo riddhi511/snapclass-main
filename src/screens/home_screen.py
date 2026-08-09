a5cab4c961c42d4f2398d63794fe554a972eb612
Riddhi <riddhisoni686@gmail.com>
2026-08-09 18:25:56 -0400

    coded

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -8,12 +8,13 @@ def home_screen():
     header_home()
     style_background_home()
     style_base_layout()
+
     col1, col2 = st.columns(2, gap="large")    
 
     with col1:
         st.header('I am Student')
         st.image("https://i.ibb.co/844D9Lrt/mascot-student.png", width=120)
-        if st.button('Student Portal', type='primary',icon=':material/arrow_outward:', icon_position='right'):
+        if st.button('Student Portal', type='primary', icon=':material/arrow_outward:', icon_position='right'):
             st.session_state['login_type'] = 'student'
             st.rerun()
         
