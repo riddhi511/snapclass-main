a78cc4dd01ae53611d116e41821c9863b3c84413
Riddhi <riddhisoni686@gmail.com>
2026-08-09 22:01:44 -0400

    coded-gcp deployement

--- src/database/config.py
+++ src/database/config.py
@@ -1,9 +1,24 @@
-import streamlit as st
+# import streamlit as st
 
+# from supabase import create_client, Client
+
+# supabase: Client = create_client(
+#     st.secrets["SUPABASE_URL"],
+#     st.secrets["SUPABASE_KEY"]
+
+# )
+
+import os
+import streamlit as st
 from supabase import create_client, Client
 
-supabase: Client = create_client(
-    st.secrets["SUPABASE_URL"],
-    st.secrets["SUPABASE_KEY"]
+def get_secret(key):
+    try:
+        return st.secrets[key]
+    except Exception:
+        return os.environ.get(key)
 
+supabase: Client = create_client(
+    get_secret("SUPABASE_URL"),
+    get_secret("SUPABASE_KEY")
 )
